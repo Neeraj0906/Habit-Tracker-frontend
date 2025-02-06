eb61dc34bd824245bc33816a9ba1c4524fe2dd5e
Neeraj <eng20cs0301.neeraj@gmail.com>
2025-02-06 18:55:19 +0530

    3 commit

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -21,7 +21,7 @@ const Signup = () => {
     e.preventDefault();
     setError('');
     try {
-      const response = await axios.post('http://localhost:5000/api/auth/register', formData);
+      const response = await axios.post(`${process.env.REACT_APP_API_URL}/api/auth/register`, formData);
       console.log('Backend Response:', response.data); // Debugging log
       navigate('/login');
     } catch (err) {
